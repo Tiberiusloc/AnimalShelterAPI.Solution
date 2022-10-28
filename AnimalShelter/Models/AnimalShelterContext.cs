1f4998dcf3e776b668d3afb36d6772b0d20a2d30
Tiberius Lockett <tlockett680@gmail.com>
2022-10-28 16:28:01 -0700

    Update AnimalShelterContext

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -8,19 +8,7 @@ namespace AnimalShelter.Models
         public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
             : base(options)
         {
-        }
-
-        
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-        base.OnModelCreating(builder);
-        builder.Entity<Animal>()
-            .HasData(
-                new Animal { AnimalId = 1, Name = "Dara", Species = "dog!!", Breed = "German Shepherd",PostDate = "16 August 2022" },
-                new Animal { AnimalId = 2, Name = "Blaze", Species = "dog", Breed = "Klee Kai", PostDate = "16 August 2022" },
-                new Animal { AnimalId = 3, Name = "Buddy", Species = "dog", Breed = "Klee Kai", PostDate = "16 August 2022" },
-                new Animal { AnimalId = 4, Name = "Dodger", Species = "cat", Breed = "Siamese", PostDate = "16 August 2022" }
-            );
+          
         }
         public DbSet<Animal> Animals { get; set; }
     }
