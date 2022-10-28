6f7490987cd5fd5959c15fccfbbde71ec768d2b3
Tiberius Lockett <tlockett680@gmail.com>
2022-10-28 14:01:32 -0700

    Add Put to AnimalsController.cs

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -60,5 +60,35 @@ namespace AnimalShelter.Controllers
 
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId}, animal);
     }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal, string name)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else 
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
   }
 }
